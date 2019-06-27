846a9759d786f6cc97edc3da526e55d02e1f3354
Oliver <ocben1@gmail.com>
2019-06-27 21:36:08 +1000

    connected UpdateAPIView subclass to router

--- demo/urls.py
+++ demo/urls.py
@@ -9,8 +9,8 @@ import store.api_views
 urlpatterns = [
     path('api/v1/products/', store.api_views.ProductList.as_view()),
     path('api/v1/products/new', store.api_views.ProductCreate.as_view()),
-    path('api/v1/products/<int:id>/destroy',
-        store.api_views.ProductDestroy.as_view()
+    path('api/v1/products/<int:id>/',
+        store.api_views.ProductRetrieveUpdateDestroy.as_view()
         ),
 
     path('admin/', admin.site.urls),
