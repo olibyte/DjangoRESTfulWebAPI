3e832c8d5e1cf783e3159b7df0231504b06511d2
Oliver <ocben1@gmail.com>
2019-06-27 19:23:04 +1000

    enabled full-text search filter back end

--- store/api_views.py
+++ store/api_views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 from store.serializers import ProductSerializer
 from store.models import Product
@@ -7,8 +8,9 @@ from store.models import Product
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
+    search_fields = ('name', 'description')
 
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
