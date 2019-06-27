3f1523fdf7222b94c595d3a7926fd03a3387a6dc
Oliver <ocben1@gmail.com>
2019-06-27 18:51:27 +1000

    filter backends with URL query parameters

--- store/migrations/0002_product_data.py
+++ store/migrations/0002_product_data.py
@@ -16,14 +16,14 @@ def create_sample_product_data(apps, schema_editor):
         id=3, name='Vitamin A 10,000 IU (125 caplets)', price=3.00,
         description='Vitamin A is essential for normal and night vision, and helps maintain healthy skin and mucous membranes.',
         sale_start=timezone.now(),
-        sale_end=timezone.now() + timedelta(days=10),
+        sale_end=timezone.now() + timedelta(days=365),
         photo='products/vitamin-a.jpg',
     ).save()
     Product(
         id=4, name='Vitamin B-Complex (100 caplets)', price=3.00,
         description='Contains a combination of essential B vitamins that help convert food to energy.',
         sale_start=timezone.now(),
-        sale_end=timezone.now() + timedelta(days=10),
+        sale_end=timezone.now() + timedelta(days=365),
         photo='products/vitamin-bcomplex.jpg',
     ).save()
 
