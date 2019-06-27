462ddfb06bf557e82d3e75fd8c6f3cfd15a890e0
Oliver <ocben1@gmail.com>
2019-06-27 19:34:16 +1000

    enabled pagination of querysets in API responses

--- store/api_views.py
+++ store/api_views.py
@@ -1,9 +1,14 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from store.serializers import ProductSerializer
 from store.models import Product
+
+class ProductsPagination(LimitOffsetPagination):
+    default_limit=10
+    max_limit=100
 
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
@@ -11,6 +16,7 @@ class ProductList(ListAPIView):
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
     search_fields = ('name', 'description')
+    pagination_class = ProductsPagination
 
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
