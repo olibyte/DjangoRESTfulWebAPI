6b5fc82b67e3b74587e7cd5cdeef5b488aa80490
Oliver <ocben1@gmail.com>
2019-06-29 22:35:15 +1000

    added test case for ListAPIView subclass

--- store/tests.py
+++ store/tests.py
@@ -38,3 +38,11 @@ class ProductDestroyTestCase(APITestCase):
             Product.DoesNotExist,
             Product.objects.get, id=product_id,
         )
+class ProductListTestCase(APITestCase):
+    def test_list_products(self):
+        products_count = Product.objects.count()
+        response = self.client.get('/api/v1/products/')
+        self.assertIsNone(response.data['next'])
+        self.assertIsNone(response.data['previous'])
+        self.assertEqual(response.data['count'], products_count)
+        self.assertEqual(len(response.data['results']), products_count)
